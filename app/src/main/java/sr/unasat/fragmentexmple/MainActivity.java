9e8e7f39cc91612b80502e0d1e23dae831f8ea74
Eric Ronde <e.ronde@unasat.sr>
2019-02-20 18:58:23 -0300

    Initial commit

---
+++ app/src/main/java/sr/unasat/fragmentexmple/MainActivity.java
@@ -0,0 +1,43 @@
+package sr.unasat.fragmentexmple;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void ChangeFragment (View view){
+        Fragment fragment;
+
+        if (view == findViewById(R.id.button)){
+            fragment = new FragmentOne();
+            FragmentManager fm = getSupportFragmentManager();
+            FragmentTransaction ft = fm.beginTransaction();
+            ft.replace(R.id.fragment_place, fragment);
+            ft.commit();
+        }
+        if (view == findViewById(R.id.button2)){
+            fragment = new FragmentTwo();
+            FragmentManager fm = getSupportFragmentManager();
+            FragmentTransaction ft = fm.beginTransaction();
+            ft.replace(R.id.fragment_place, fragment);
+            ft.commit();
+        }
+        if (view == findViewById(R.id.button3)){
+            fragment = new FragmentThree();
+            FragmentManager fm = getSupportFragmentManager();
+            FragmentTransaction ft = fm.beginTransaction();
+            ft.replace(R.id.fragment_place, fragment);
+            ft.commit();
+        }
+    }
+}
